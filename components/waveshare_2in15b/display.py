220251abf65cca26e8cd523419a792b0755ff6d2
DrownedReaper <james.ham@bigpond.com>
2026-03-30 22:01:48 +1100

    Add Pin Configuration for YAML

--- components/waveshare_2in15b/display.py
+++ components/waveshare_2in15b/display.py
@@ -3,6 +3,11 @@ import esphome.config_validation as cv
 
 from esphome.components import display, spi
 from esphome.const import CONF_ID
+from esphome.components import gpio
+
+CONF_DC_PIN = "dc_pin"
+CONF_RESET_PIN = "reset_pin"
+CONF_BUSY_PIN = "busy_pin"
 
 waveshare_ns = cg.esphome_ns.namespace("waveshare")
 Waveshare2in15B = waveshare_ns.class_(
@@ -11,14 +16,30 @@ Waveshare2in15B = waveshare_ns.class_(
     spi.SPIDevice,
 )
 
+
 CONFIG_SCHEMA = display.FULL_DISPLAY_SCHEMA.extend(
-    spi.spi_device_schema()
-).extend(
-    {cv.GenerateID(): cv.declare_id(Waveshare2in15B)}
-)
+    {
+        cv.GenerateID(): cv.declare_id(Waveshare2in15B),
+        cv.Optional(CONF_DC_PIN): gpio.gpio_output_pin_schema,
+        cv.Optional(CONF_RESET_PIN): gpio.gpio_output_pin_schema,
+        cv.Optional(CONF_BUSY_PIN): gpio.gpio_input_pin_schema,
+    }
+).extend(spi.spi_device_schema())
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
 
     await display.register_display(var, config)
     await spi.register_spi_device(var, config)
+
+    if CONF_DC_PIN in config:
+        dc = await gpio.gpio_output_pin_expression(config[CONF_DC_PIN])
+        cg.add(var.set_dc_pin(dc))
+
+    if CONF_RESET_PIN in config:
+        rst = await gpio.gpio_output_pin_expression(config[CONF_RESET_PIN])
+        cg.add(var.set_reset_pin(rst))
+
+    if CONF_BUSY_PIN in config:
+        busy = await gpio.gpio_input_pin_expression(config[CONF_BUSY_PIN])
+        cg.add(var.set_busy_pin(busy))

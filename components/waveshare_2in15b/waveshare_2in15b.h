f5f45b91b27d9330ab8ba707db2c74ccab6e65db
DrownedReaper <james.ham@bigpond.com>
2026-03-30 22:04:15 +1100

    Added Pins for YAML

--- components/waveshare_2in15b/waveshare_2in15b.h
+++ components/waveshare_2in15b/waveshare_2in15b.h
@@ -2,6 +2,7 @@
 
 #include "esphome/components/display/display_buffer.h"
 #include "esphome/components/spi/spi.h"
+#include "esphome/components/gpio/gpio.h"
 #include "esphome/core/log.h"
 
 namespace esphome {
@@ -15,12 +16,21 @@ class Waveshare2in15B
   void setup() override;
   void update() override;
   void fill(Color color) override;
+  void set_dc_pin(GPIOPin *pin) { dc_pin_ = pin; }
+  void set_reset_pin(GPIOPin *pin) { reset_pin_ = pin; }
+  void set_busy_pin(GPIOPin *pin) { busy_pin_ = pin; }
+
 
  protected:
   void draw_absolute_pixel_internal(int x, int y, Color color) override;
 
   int get_width_internal() override { return 296; }
   int get_height_internal() override { return 160; }
+
+  GPIOPin *dc_pin_{nullptr};
+  GPIOPin *reset_pin_{nullptr};
+  GPIOPin *busy_pin_{nullptr};
+
 
  private:
   uint8_t buffer_black_[296 * 160 / 8];

5ab5ec01a68be6febed2ac85ac53419d32ef9c1d
DrownedReaper <james.ham@bigpond.com>
2026-04-05 13:15:42 +1000

    Add files via upload

--- components/waveshare_2in15b/display.py
+++ components/waveshare_2in15b/display.py
@@ -1,44 +1,52 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-
-from esphome.components import display, spi
 from esphome import pins
-from esphome.const import CONF_ID
+from esphome.components import display, spi
+from esphome.const import (
+    CONF_BUSY_PIN,
+    CONF_DC_PIN,
+    CONF_ID,
+    CONF_LAMBDA,
+    CONF_RESET_PIN,
+    CONF_UPDATE_INTERVAL,
+)
 
-CONF_DC_PIN = "dc_pin"
-CONF_RESET_PIN = "reset_pin"
-CONF_BUSY_PIN = "busy_pin"
-CONF_POWER_PIN = "power_pin"
+DEPENDENCIES = ["spi"]
+AUTO_LOAD = ["display"]
 
-waveshare_ns = cg.esphome_ns.namespace("waveshare")
-Waveshare2in15B = waveshare_ns.class_(
-    "Waveshare2in15B",
-    cg.Component,
+# Namespace MUST match the C++ namespace exactly
+waveshare_2in15b_ns = cg.esphome_ns.namespace("waveshare_2in15b")
+
+# Class name MUST match the C++ class name exactly
+WaveshareEPaper2in15B = waveshare_2in15b_ns.class_(
+    "WaveshareEPaper2in15B",
     display.DisplayBuffer,
+    spi.SPIDevice,
+    cg.PollingComponent,
 )
 
-CONFIG_SCHEMA = display.FULL_DISPLAY_SCHEMA.extend(
-    {
-        cv.GenerateID(): cv.declare_id(Waveshare2in15B),
-
-        cv.Optional(CONF_DC_PIN): pins.gpio_output_pin_schema,
-        cv.Optional(CONF_RESET_PIN): pins.gpio_output_pin_schema,
-        cv.Optional(CONF_BUSY_PIN): pins.gpio_input_pin_schema,
-        cv.Optional(CONF_POWER_PIN): pins.gpio_output_pin_schema,
-    }
-).extend(
-    spi.spi_device_schema()
+CONFIG_SCHEMA = (
+    display.BASIC_DISPLAY_SCHEMA.extend(
+        {
+            cv.GenerateID(): cv.declare_id(WaveshareEPaper2in15B),
+            cv.Required(CONF_DC_PIN): pins.gpio_output_pin_schema,
+            cv.Optional(CONF_RESET_PIN): pins.gpio_output_pin_schema,
+            cv.Optional(CONF_BUSY_PIN): pins.gpio_input_pin_schema,
+        }
+    )
+    .extend(cv.polling_component_schema("300s"))
+    .extend(spi.spi_device_schema(cs_pin_required=True))
 )
+
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-
+    await cg.register_component(var, config)
     await display.register_display(var, config)
     await spi.register_spi_device(var, config)
 
-    if CONF_DC_PIN in config:
-        dc = await cg.gpio_pin_expression(config[CONF_DC_PIN])
-        cg.add(var.set_dc_pin(dc))
+    dc = await cg.gpio_pin_expression(config[CONF_DC_PIN])
+    cg.add(var.set_dc_pin(dc))
 
     if CONF_RESET_PIN in config:
         rst = await cg.gpio_pin_expression(config[CONF_RESET_PIN])
@@ -48,7 +56,10 @@ async def to_code(config):
         busy = await cg.gpio_pin_expression(config[CONF_BUSY_PIN])
         cg.add(var.set_busy_pin(busy))
 
-    if CONF_POWER_PIN in config:
-        pwr = await cg.gpio_pin_expression(config[CONF_POWER_PIN])
-        cg.add(var.set_power_pin(pwr))
-
+    if CONF_LAMBDA in config:
+        lambda_ = await cg.process_lambda(
+            config[CONF_LAMBDA],
+            [(display.DisplayRef, "it")],
+            return_type=cg.void,
+        )
+        cg.add(var.set_writer(lambda_))

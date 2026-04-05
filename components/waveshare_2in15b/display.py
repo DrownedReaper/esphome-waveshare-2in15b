6031317790778f84cae2248f39d8f92892b8e8a4
DrownedReaper <james.ham@bigpond.com>
2026-04-06 09:04:55 +1000

    Add files via upload

--- components/waveshare_2in15b/display.py
+++ components/waveshare_2in15b/display.py
@@ -8,11 +8,12 @@ from esphome.const import (
     CONF_ID,
     CONF_LAMBDA,
     CONF_RESET_PIN,
-    CONF_ROTATION,
 )
 
 DEPENDENCIES = ["spi"]
 AUTO_LOAD = ["display"]
+
+CONF_ROTATION = "rotation"
 
 waveshare_2in15b_ns = cg.esphome_ns.namespace("waveshare_2in15b")
 
@@ -23,6 +24,15 @@ WaveshareEPaper2in15B = waveshare_2in15b_ns.class_(
     cg.PollingComponent,
 )
 
+DisplayRotation = display.display_ns.enum("DisplayRotation")
+
+ROTATIONS = {
+    0:   DisplayRotation.DISPLAY_ROTATION_0_DEGREES,
+    90:  DisplayRotation.DISPLAY_ROTATION_90_DEGREES,
+    180: DisplayRotation.DISPLAY_ROTATION_180_DEGREES,
+    270: DisplayRotation.DISPLAY_ROTATION_270_DEGREES,
+}
+
 CONFIG_SCHEMA = (
     display.BASIC_DISPLAY_SCHEMA.extend(
         {
@@ -30,6 +40,7 @@ CONFIG_SCHEMA = (
             cv.Required(CONF_DC_PIN): pins.gpio_output_pin_schema,
             cv.Optional(CONF_RESET_PIN): pins.gpio_output_pin_schema,
             cv.Optional(CONF_BUSY_PIN): pins.gpio_input_pin_schema,
+            cv.Optional(CONF_ROTATION, default=0): cv.one_of(0, 90, 180, 270, int=True),
         }
     )
     .extend(cv.polling_component_schema("300s"))
@@ -52,6 +63,9 @@ async def to_code(config):
     if CONF_BUSY_PIN in config:
         busy = await cg.gpio_pin_expression(config[CONF_BUSY_PIN])
         cg.add(var.set_busy_pin(busy))
+
+    rotation = config[CONF_ROTATION]
+    cg.add(var.set_rotation(ROTATIONS[rotation]))
 
     if CONF_LAMBDA in config:
         lambda_ = await cg.process_lambda(
